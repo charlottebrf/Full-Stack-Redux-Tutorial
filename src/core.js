9037072fc01020e908643e404cb1a61598ed4fcd
Charlotte Fereday <cfereday@thoughtworks.com>
2018-03-04 19:24:51 +0000

    [Charlotte] uses immutable lib functions to merge an update into old state & create new set of entries

--- src/core.js
+++ src/core.js
@@ -1,6 +1,15 @@
 let immutable = require('immutable');
 let List = immutable.List;
+let Map = immutable.Map;
 
 export function setEntries(state, entries) {
     return state.set('entries', List(entries));
+}
+
+export function next(state) {
+    const entries = state.get('entries');
+    return state.merge({
+       vote: Map({pair: entries.take(2)}),
+       entries: entries.skip(2)
+    });
 }
--- test/core_spec.js
+++ test/core_spec.js
@@ -3,19 +3,22 @@ let expect = chai.expect;
 let immutable = require('immutable');
 let List = immutable.List;
 let Map = immutable.Map;
-
-import {setEntries} from '../src/core';
+import {setEntries, next} from '../src/core';
 
 describe('application logic', () => {
 
     describe('setEntries', () => {
 
         it('adds the entries to the state', () => {
-            const state = Map();
-            const entries = ['Trainspotting', '28 Days Later'];
-            const nextState = setEntries(state, entries);
+            const state = Map({
+                entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+            });
+            const nextState = next(state);
             expect(nextState).to.equal(Map({
-                entries: List.of('Trainspotting', '28 Days Later')
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later')
+                }),
+                entries: List.of('Sunshine')
             }));
         });
     });
